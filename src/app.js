73547a9429ddf6d7b3f33ec814d4964e90afb89a
unknown <emma.pavelak@gmail.com>
2021-05-23 16:56:34 +0200

    database co

--- src/app.js
+++ src/app.js
@@ -17,4 +17,11 @@ app.use(loggerMiddleware);
 app.use('/api', router);
 app.use(errorMiddleware);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.listen(3000);
+
--- src/common/database/database.config.js
+++ src/common/database/database.config.js
@@ -1,15 +1,13 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('messagewall', 'root', 'Lou974Lou!', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-/*
-const sequelize = new Sequelize('ynov_final', 'ynov', 'oT4mI6fZ0bdX5d', {
+/*const sequelize = new Sequelize('messagewall', 'root', 'Lou974Lou!', {
   host: 'localhost',
   dialect: 'mysql'
 });*/
-
+const sequelize = new Sequelize('ynov_final', 'ynov', 'oT4mI6fZ0bdX5d', {
+  host: 'btcv.fr',
+  dialect: 'mysql'
+});
 
 sequelize
   .authenticate()
